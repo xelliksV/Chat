b1725d83aef2e6f45cbf06a20dbfd4a66e4172f3
Vladislav Pashkov <xelliks666@gmail.com>
2025-01-22 16:46:24 +0700

    Realeased sending messages

--- Server.java
+++ Server.java
@@ -3,8 +3,11 @@ package com.javarush.task.task30.task3008;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class Server {
+    private static Map<String, Connection> connectionMap = new ConcurrentHashMap<>();
     public static void main(String[] args) throws IOException {
         ConsoleHelper.writeMessage("Введите порт сервера: ");
         int port = ConsoleHelper.readInt();
@@ -22,6 +25,15 @@ public class Server {
         private Socket socket;
         public Handler(Socket socket) {
             this.socket = socket;
+        }
+    }
+    public static void sendBroadcastMessage(Message message) {
+        for (Map.Entry<String, Connection> entry : connectionMap.entrySet()) {
+            try {
+                entry.getValue().send(message);
+            } catch (IOException e) {
+                ConsoleHelper.writeMessage("Не удалось отправить сообщение");
+            }
         }
     }
 }

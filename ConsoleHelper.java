2806acb8e61a3f1ddc3cfa48d410a5f3820b047a
Vladislav Pashkov <xelliks666@gmail.com>
2025-01-22 13:37:47 +0700

    working

--- ConsoleHelper.java
+++ ConsoleHelper.java
@@ -1,4 +1,31 @@
 package com.javarush.task.task30.task3008;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class ConsoleHelper {
+    private static BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+    public static void writeMessage(String message) {
+        System.out.println(message);
+    }
+    public static String readString() {
+        while (true) {
+            try {
+                String buf = reader.readLine();
+                if (buf != null)
+                    return buf;
+            } catch (IOException e) {
+                writeMessage("Произошла ошибка при попытке ввода текста. Попробуйте еще раз.");
+            }
+        }
+    }
+    public static int readInt() {
+        try {
+            return Integer.parseInt(readString());
+        } catch (NumberFormatException e) {
+            System.out.println("Произошла ошибка при попытке ввода числа. Попробуйте еще раз.");
+            return Integer.parseInt(readString());
+        }
+    }
 }
